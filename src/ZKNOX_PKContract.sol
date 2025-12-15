9ada5c1e9eea57b61ddb5a629b5c1100eb893876
Reno <renaud.dubois.fr@gmail.com>
2025-12-15 09:29:10 +0100

    pkcontract in asm

--- src/ZKNOX_PKContract.sol
+++ src/ZKNOX_PKContract.sol
@@ -51,6 +51,98 @@ contract PKContract {
     function get_mldsa_public_key() external view returns (PubKey memory) {
         return mldsa_pk;
     }
+
+    function Deploy_Kpub(
+    uint256[][][] memory _a_hat, 
+    bytes memory _tr, 
+    uint256[][] memory _t1, 
+    uint256 salt
+    ) external returns (address a_polynomial) {
+    
+    // Build the payload by flattening everything
+    bytes memory payload;
+    
+    // Flatten a_hat (k x l x 256)
+    for (uint256 i = 0; i < _a_hat.length; i++) {
+        for (uint256 j = 0; j < _a_hat[i].length; j++) {
+            payload = abi.encodePacked(payload, _a_hat[i][j]);
+        }
+    }
+    
+    // Append tr
+    payload = abi.encodePacked(payload, _tr);
+    
+    // Flatten t1 (k x 256)
+    for (uint256 i = 0; i < _t1.length; i++) {
+        payload = abi.encodePacked(payload, _t1[i]);
+    }
+    
+    // Wrap with creation code
+    bytes memory bytecode_pol = abi.encodePacked(
+        hex"63", 
+        uint32(payload.length), 
+        hex"80600E6000396000F3", 
+        payload
+    );
+    
+    assembly {
+        a_polynomial := create2(0, add(bytecode_pol, 0x20), mload(bytecode_pol), salt)
+    }
+    require(a_polynomial != address(0), "Deployment failed");
+}
+
+function Deploy_flatten_Kpub(
+    bytes calldata flatPayload,  // pre-encoded off-chain
+    uint256 salt
+) external returns (address a_polynomial) {
+    bytes memory bytecode_pol = abi.encodePacked(
+        hex"63", 
+        uint32(flatPayload.length), 
+        hex"80600E6000396000F3", 
+        flatPayload
+    );
+    
+    assembly {
+        a_polynomial := create2(0, add(bytecode_pol, 0x20), mload(bytecode_pol), salt)
+    }
+    require(a_polynomial != address(0), "Deployment failed");
+}
+
+function ReadPolynomial(address pkContract) 
+    external view 
+    returns (
+        uint256[4][4][32] memory a_hat,
+        bytes32 tr,
+        uint256[4][32] memory t1,
+        bytes memory salt
+    ) 
+{
+    // Sizes:
+    // a_hat: 4 * 4 * 32 * 32 = 16384 bytes (0x4000)
+    // tr: 32 bytes (0x20)
+    // t1: 4 * 32 * 32 = 4096 bytes (0x1000)
+    // salt: 40 bytes (0x28)
+    // Total: 20552 bytes (0x5048)
+    
+    salt = new bytes(40);
+    
+    assembly {
+        // a_hat: bytes 0 to 16383
+        extcodecopy(pkContract, a_hat, 0, 0x4000)
+        
+        // tr: bytes 16384 to 16415 (use scratch space)
+        extcodecopy(pkContract, 0x00, 0x4000, 0x20)
+        tr := mload(0x00)
+        
+        // t1: bytes 16416 to 20511
+        extcodecopy(pkContract, t1, 0x4020, 0x1000)
+        
+        // salt: bytes 20512 to 20551 (40 bytes)
+        mstore(salt, 0x28)  // set length to 40
+        extcodecopy(pkContract, add(salt, 0x20), 0x5020, 0x28)
+    }
+}
+
 }
 
 interface IPKContract {
