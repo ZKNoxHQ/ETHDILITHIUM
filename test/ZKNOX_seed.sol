7053d4a0941c4b743bfd400e418694a04215061d
Simon Masson <simon.masson@protonmail.com>
2025-12-19 16:46:58 +0100

    remove useless variable

--- test/ZKNOX_seed.sol
+++ test/ZKNOX_seed.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 library Constants {
     uint256 constant SEED_PREQUANTUM = 0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef;
-    string constant SEED_PREQUANTUM_STR = "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef";
     address constant ADDR_PREQUANTUM = 0xC96aAa54E2d44c299564da76e1cD3184A2386B8D;
     uint256 constant SEED_POSTQUANTUM = 0xcafecafecafecafecafecafecafecafecafecafecafecafecafecafecafecafe;
     string constant SEED_POSTQUANTUM_STR = "cafecafecafecafecafecafecafecafecafecafecafecafecafecafecafecafe";
