c29c46e3a80c08e353733d882a824a2a63857bfb
Simon Masson <simon.masson@protonmail.com>
2025-12-30 15:53:24 +0100

    better parsing

--- pythonref/setup.py
+++ pythonref/setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+def parse_requirements(filename):
+    """Parse requirements.txt, separating regular packages from git URLs"""
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    
+    regular_requires = []
+    for line in lines:
+        line = line.strip()
+        if line and not line.startswith('#'):
+            # Skip git+ URLs, they need to be installed separately
+            if not line.startswith('git+') and not line.startswith('-e git+'):
+                regular_requires.append(line)
+    
+    return regular_requires
 
 setup(
     name="dilithium-py",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=parse_requirements('requirements.txt'),
 )
