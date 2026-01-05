ca4041a8f6a7918ed157a8ed2666d77fed083bef
Simon Masson <simon.masson@protonmail.com>
2026-01-05 17:58:29 +0100

    new lines

--- src/ZKNOX_PKContract.sol
+++ src/ZKNOX_PKContract.sol
@@ -7,16 +7,16 @@ import {PubKey} from "./ZKNOX_dilithium_utils.sol";
 /// @title PKContractV2 - Optimized ML-DSA Public Key Storage
 /// @notice Stores an expanded ML-DSA public key using SSTORE2 for gas-efficient deployment
 /// @dev Uses bytecode storage pattern to reduce deployment costs by ~70%
-contract PKContract{
+contract PKContract {
     /// @notice Pointer to contract storing A_hat matrix as bytecode
     address private immutable aHatPointer;
-    
+
     /// @notice Pointer to contract storing t1 vector as bytecode
     address private immutable t1Pointer;
-    
+
     /// @notice First 32 bytes of tr
     bytes32 private immutable trPart1;
-    
+
     /// @notice Second 32 bytes of tr
     bytes32 private immutable trPart2;
 
@@ -26,11 +26,11 @@ contract PKContract{
     /// @param _t1 t1 vector (uint256[4][32])
     constructor(uint256[][][] memory _aHat, bytes memory _tr, uint256[][] memory _t1) {
         require(_tr.length == 64, "tr must be 64 bytes");
-        
+
         // Encode and store large arrays as contract bytecode via SSTORE2
         aHatPointer = SSTORE2.write(abi.encode(_aHat));
         t1Pointer = SSTORE2.write(abi.encode(_t1));
-        
+
         // Store tr in immutable slots (cheaper than SSTORE2 for small data)
         bytes32 part1;
         bytes32 part2;
@@ -48,11 +48,11 @@ contract PKContract{
         // Decode A_hat from bytecode storage
         bytes memory aHatData = SSTORE2.read(aHatPointer);
         uint256[][][] memory aHat = abi.decode(aHatData, (uint256[][][]));
-        
+
         // Decode t1 from bytecode storage
         bytes memory t1Data = SSTORE2.read(t1Pointer);
         uint256[][] memory t1 = abi.decode(t1Data, (uint256[][]));
-        
+
         // Reconstruct tr from immutable slots
         bytes memory tr = new bytes(64);
         bytes32 part1 = trPart1;
@@ -61,7 +61,7 @@ contract PKContract{
             mstore(add(tr, 32), part1)
             mstore(add(tr, 64), part2)
         }
-        
+
         return PubKey({aHat: aHat, tr: tr, t1: t1});
     }
 }
